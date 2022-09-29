b7ed86b23f8ceb94d22496f4a19f5c1a4eafc711
Basil Khan <basilkhan76@gmail.com>
2022-09-29 17:16:12 -0400

    Project Init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,19 @@ export default function Home() {
   return (
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
-        <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
-        </h1>
+        <h1 className="text-blue-500 ">Hello</h1>
+        {/* {Header} */}
+
+        {/* {Hero} */}
+
+        {/* {About} */}
+
+        {/* {Experience} */}
+
+        {/* {Skills} */}
+
+        {/* {Projects} */}
       </main>
     </div>
-  )
+  );
 }
